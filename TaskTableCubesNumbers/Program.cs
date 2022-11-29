e8cbed735038fcc33ab95d3cba1b5cd96b79d2d2
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-29 09:11:12 +0700

    adding of task_Cubes_Numbers

---
+++ TaskTableCubesNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Input the integer positive number");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"The list of cubes of numbers from 1 to {n} is: (");
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n) Console.Write($" {Math.Pow(i, 3)})");
+        else Console.Write($" {Math.Pow(i, 3)},");
+    }
+}
+catch
+{
+    Console.WriteLine("You should input the integer positive number only!!!");
+}
